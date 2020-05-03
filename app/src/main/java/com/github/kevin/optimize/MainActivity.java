5031b5c29412621f7012d6bd2eb161da1889b782
kevin <duxianguo_cd@keruyun.com>
2020-05-03 16:25:42 +0800

    创建项目

---
+++ app/src/main/java/com/github/kevin/optimize/MainActivity.java
@@ -0,0 +1,21 @@
+package com.github.kevin.optimize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void click(View view) {
+        Toast.makeText(this,"被点击了",Toast.LENGTH_SHORT).show();
+    }
+}
